12aee4b6f81fb00d9649c3c9cfe7141151203e67
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2019-02-27 16:54:54 +0800

    replace 0 with null

--- primary/tree_question_test.cpp
+++ primary/tree_question_test.cpp
@@ -2,6 +2,8 @@
 #include "../main.h"
 #include "../util.h"
 
+#define null 0
+
 TreeNode* build_tree(vector<int> nums){
     if (nums.size() == 0){
         return nullptr;
@@ -27,20 +29,25 @@ TreeNode* build_tree(vector<int> nums){
 }
 
 void test_maxDepth(){
-    cout << maxDepth(build_tree({5,1,4,0,0,3,6})) << endl;
+    cout << maxDepth(build_tree({5,1,4,null,null,3,6})) << endl;
     cout << maxDepth(build_tree({2,1,3})) << endl;
 }
 
 void test_isValidBST(){
-    cout << isValidBST(build_tree({5,1,4,0,0,3,6})) << endl;
+    cout << isValidBST(build_tree({5,1,4,null,null,3,6})) << endl;
     cout << isValidBST(build_tree({2,1,3})) << endl;
-    cout << isValidBST(build_tree({10,5,15,0,0,6,20})) << endl;
+    cout << isValidBST(build_tree({10,5,15,null,null,6,20})) << endl;
 }
 
 void test_isValidBST2(){
-    cout << isValidBST2(build_tree({5,1,4,0,0,3,6})) << endl;
+    cout << isValidBST2(build_tree({5,1,4,null,null,3,6})) << endl;
     cout << isValidBST2(build_tree({2,1,3})) << endl;
-    cout << isValidBST2(build_tree({10,5,15,0,0,6,20})) << endl;
+    cout << isValidBST2(build_tree({10,5,15,null,null,6,20})) << endl;
+}
+
+void test_isSymmetric(){
+    cout << isSymmetric(build_tree({1,2,2,3,4,4,3})) <<endl;
+    cout << isSymmetric(build_tree({1,2,2,null,3,null,3})) <<endl;    
 }
 
 int func(){
