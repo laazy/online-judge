b25982a98bd59aeeade324e434645df9162829fc
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2019-03-02 16:20:02 +0800

    rob

--- primary/dp_question.cpp
+++ primary/dp_question.cpp
@@ -23,10 +23,14 @@ int maxProfit(vector<int>& prices) {
 }
 
 /**
- * 这道题难度很大，核心思想是这样：
+ * 这道题很经典，核心思想是这样：
  * 构造一个函数 f(n)，为以第n个数为结尾的最大子序列和
  * 很容易可以发现， 当f(n-1)已知时，f(n) = max(f(n-1) + A[n], A[n])
- * 这样就可以在一次遍历中，找到最大的一个子序列和
+ * 这样就可以在一次遍历中，找到最大的一个子序列
+ * 
+ * 这道题的状态可以是 以n为结尾的最大子序列
+ * 所以状态转移方程就是上述公式
+ * 边界值自然就是第一个数
  */ 
 int maxSubArray(vector<int>& nums) {
     if (nums.empty()){
@@ -42,22 +46,32 @@ int maxSubArray(vector<int>& nums) {
     return ans;
 }
 
+/**
+ * 
+ * 考虑F(n)为偷窃到第n家，那么记录最后一次偷窃的为m
+ *         / max(F(n-1), F(n-2) + A[n]), n == m   
+ * F(n) = <
+ *         \ F(n-1) + A[n], n != m 
+ * 有一个细节，如果F(n-1) == F(n-2) + A[n]时， 应当选取前一个，因为这会给后续留下空间，减少第一步执行次数
+ */ 
 int rob(vector<int>& nums) {
     if (nums.empty()){
         return 0;
     }
-    int total = nums[0];
-    size_t last = 0;
-    for (size_t i = 1; i < nums.size(); i++){
-        if (last == i - 1){
-            if (total < total - nums[last] + nums[i]){
-                last = i;
-                total += nums[i] - nums[last];
-            }
-        }else{
-            total += nums[i];
+    int fn = 0, fn1 = 0, fn2 = 0; 
+    size_t last = -2;
+    for (size_t i = 0; i < nums.size(); i++){
+        if (last != i - 1){
+            fn = fn1 + nums[i];
+            last = i;
+        } else if (fn1 >= fn2 + nums[i]){
+            fn = fn1;
+        } else {
+            fn = fn2 + nums[i];
             last = i;
         }
+        fn2 = fn1;
+        fn1 = fn;
     }
-    return total;
+    return fn;
 }
