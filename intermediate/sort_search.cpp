5580b80f6b15d122d77bc42519aa997d157b7eed
root <root@Necronomicon.localdomain>
2019-08-31 14:02:31 +0800

    format

--- intermediate/sort_search.cpp
+++ intermediate/sort_search.cpp
@@ -1,16 +1,16 @@
 #include "sort_search.h"
-#include <unordered_map>
 #include <queue>
-
+#include <unordered_map>
 
 /**
- * 这里的ptr 和 ptr_2加起来扫描了一次数组，ptr_0只是为了简化代码，实际上可以采用记录1的数量cnt_1，然后ptr - cnt_1来确定ptr_0
- */ 
-void sortColors(vector<int>& nums){
+ * 这里的ptr 和
+ * ptr_2加起来扫描了一次数组，ptr_0只是为了简化代码，实际上可以采用记录1的数量cnt_1，然后ptr
+ * - cnt_1来确定ptr_0
+ */
+void sortColors(vector<int> &nums) {
     auto ptr_0 = nums.begin(), ptr_2 = nums.end();
-    for (auto ptr = nums.begin(); ptr != ptr_2;){
-        switch (*ptr)
-        {
+    for (auto ptr = nums.begin(); ptr != ptr_2;) {
+        switch (*ptr) {
             case 0:
                 swap(*ptr, *ptr_0);
                 ptr_0++;
@@ -27,17 +27,17 @@ void sortColors(vector<int>& nums){
     }
 }
 
-vector<int> topKFrequent(vector<int>& nums, int k) {
+vector<int> topKFrequent(vector<int> &nums, int k) {
     unordered_map<int, int> cnt;
     priority_queue<pair<int, int>> pq;
     vector<int> ans;
-    for (int &i : nums){
+    for (int &i : nums) {
         cnt[i]++;
     }
-    for (auto i: cnt){
+    for (auto i : cnt) {
         pq.push(pair<int, int>{i.second, i.first});
     }
-    while (k > 0){
+    while (k > 0) {
         ans.push_back(pq.top().second);
         pq.pop();
         k--;
@@ -45,28 +45,30 @@ vector<int> topKFrequent(vector<int>& nums, int k) {
     return ans;
 }
 
-int findKthLargest_helper(vector<int> &nums, int k, int begin, int end){
+int findKthLargest_helper(vector<int> &nums, int k, int begin, int end) {
     int head = nums[0];
-    int i = 1, j = end; 
-    for(;;){
-        for (; i <= end && nums[i] <= head; i++);
-        for (; j > begin && nums[j] >= head; j--);
+    int i = 1, j = end;
+    for (;;) {
+        for (; i <= end && nums[i] <= head; i++)
+            ;
+        for (; j > begin && nums[j] >= head; j--)
+            ;
         swap(nums[i], nums[j]);
-        if (j < i){
+        if (j < i) {
             swap(i, j);
             break;
         }
     }
-    swap(nums[0], nums[i]); 
-    if (i == k){
+    swap(nums[0], nums[i]);
+    if (i == k) {
         return head;
-    }else if (i < k){
+    } else if (i < k) {
         return findKthLargest_helper(nums, k - j, j, end);
-    }else{
+    } else {
         return findKthLargest_helper(nums, k, begin, i - 1);
     }
 }
 
-int findKthLargest(vector<int>& nums, int k) {
+int findKthLargest(vector<int> &nums, int k) {
     return findKthLargest_helper(nums, k - 1, 0, nums.size() - 1);
 }
